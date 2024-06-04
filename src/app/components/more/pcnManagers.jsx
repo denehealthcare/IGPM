ac3b58d34e88a47440f0822accb39165435d1e22
sgledhill1695 <carnott@denehc.co.uk>
2024-06-04 11:28:17 +0100

    framework button added to pcn managers

--- src/app/components/more/pcnManagers.jsx
+++ src/app/components/more/pcnManagers.jsx
@@ -14,6 +14,11 @@ export default function PcnManagers(){
 
                 <p className="mt-5">If you would like to apply for accreditation, please contact us, and we can arrange to raise an invoice. You can read more about accreditation <Link href="/accreditation" className="text-blue-600 hover:text-blue-800">here</Link>.</p>
 
+                <a href="https://dashboard.igpm.org.uk/wp-content/uploads/2024/06/IGPM-Accreditation-Framework-England-FINAL.pdf">
+                    <button className="bg-[#1C4F70] hover:bg-[#72C6ED] px-[29px] py-[9px] text-white rounded-[13px] text-[15px] hover:bg-[#83C5E9]:flex mb-5 mt-5">Accreditation Framework</button>
+                </a>
+
+
             </div>
 
         </>
