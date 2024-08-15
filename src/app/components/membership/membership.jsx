f091ce7337f8a46839216315345b9ffaf3c5bfd2
TonyVipros <chris@gorillamail.co.uk>
2024-08-15 09:30:20 +0100

    Update membership.jsx
    Membership page update

--- src/app/components/membership/membership.jsx
+++ src/app/components/membership/membership.jsx
@@ -8,38 +8,46 @@ export default function Membership(){
 
                 <div className="col-span-1">
 
-                    <p>The cost of Associate membership is £50 plus VAT a year (£60).</p>
+                    <h3 class="text-[1.56rem] text[#484848] mb-5">Associate Membership</h3>
+                    <p>Associate membership is open to all Managers working within General Practice throughout the UK.</p>
+                    <p>Our Inclusion Criteria can be found here:</p>
+                    <a href="https://igpm.org.uk/more/membership-inclusion-criteria">
+                                <button className="mt-5 bg-[#1C4F70] hover:bg-[#71C6EC] py-[10px] px-[20px] text-white rounded-[8px]">Membership Inclusion Criteria</button>
+                    </a>
+                    <p>Benefits include but are not limited to:</p>
+                    <ul className="list-disc ps-10 flex flex-col gap-1 mt-3">
+                        <li>The IGPM Whatsapp community - Peer to peer support on a range of topics</li>
+                        <li>The IGPM Facebook group - Exclusively for Associate and MIGPM members</li>
+                        <li>Our Team of Regional Representatives - Access to local support</li>
+                        <li>Our Accreditation process - Professional registration</li>
+                        <li>IGPM Toolkits - Access to our members only Toolkits</li>
+                        <li>Regular Webinars on a  range of topics</li>
+                    </ul>
 
-                    <p className="mt-5">
-                        You do not need a PayPal account to pay online. Alternatively please do email the office for an invoice to pay via BACS should you prefer.
-                    </p>
+                    <h3 class="text-[1.56rem] text[#484848] mb-5">Affiliate Membership</h3>
+                    <p>Affiliate Membership is available to those in arms length organisations who wish to support IGPM e.g Federations.</p>
+                    <h3 class="text-[1.56rem] text[#484848] mb-5">MIGPM Membership</h3>
+                    <p>MIGPM Membership, and a place on our professional register, is awarded to Associate members upon successful completion of our accreditation process.</p>
+                    <p>They must have had a minimum or two years relevant experience to apply.</p>
+                    <p>Being awarded MIGPM membership demonstrates that the manager works to a high standard across the wide range of skills required by the role.</p>
+                    <p>To learn more about our Accreditation process please visit our Accreditation page.</p>
 
-                    <p className="mt-5">
-                        The ‘Inclusion Criteria’ can be found here.
-                    </p>
-
-                    <p className="mt-5">
-                        To become an Associate member of the Institute of General Practice Management (IGPM) for £50 plus VAT please click on one of the buttons below.
-                    </p>
-
-                    <p className="mt-5">
-                        Once you have paid, please do email info@igpm.org.uk from your @NHS email address so we can verify your membership. Please include your surgery name and postcode in your email. Thank you.
-                    </p>
+                    <h3 class="text-[1.56rem] text[#484848] mb-5">Membership Fee</h3>
+                    <p>The cost of Associate or Affiliate membership is £50 plus VAT a year (£60) and renews annually.</p>
+                    <p>You do not need a PayPal account to pay online.</p>
+                    <p>Alternatively you can email the office for an invoice to pay via BACS should you prefer.</p>
+                    <p>Once you have paid, please email info@igpm.org.uk from your @NHS email address. Please include your surgery name and postcode in your email so we can verify your membership. Thank you.</p>
 
                 </div>
-
-
-
 
                 <div className="flex flex-col items-center">
 
                     <img src="/images/membership-group.png" alt="image of a group of people smiling" className="sm:max-w-[500px]"></img>
 {/*                     <a target="_blank" className="w-[100%] sm:max-w-[500px] lg:w-[100%]" href="https://www.paypal.com/webapps/hermes?token=32R33450PE0069346&useraction=commit&wpsFlowRedirectToXorouterSkipHermesStartTime=1697029841117&mfid=1697029840826_f7309533ee478"><button className="bg-[#1B4E70] hover:bg-[#72C6ED] hover:text-[black] w-[100%] sm:max-w-[500px] lg:w-[100%] py-[8px] rounded-[8px] mt-3 text-white">Buy Now</button></a>
  */}
-                <div className="mt-5">
-                    <PayPalButton />
-                </div>
-
+                    <div className="mt-5">
+                        <PayPalButton />
+                    </div>
 
                 </div>
 
