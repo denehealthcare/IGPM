e561d4f3ac0eb9d881b2405d8d2f7060814c75ef
TonyVipros <chris@gorillamail.co.uk>
2024-08-15 08:46:42 +0100

    Update footer.jsx
    Footer Updates

--- src/app/components/layout/footer/footer.jsx
+++ src/app/components/layout/footer/footer.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer(){
 
     const more =[
-        {title: 'Accreditation', href: '/accreditation' },
         {title: 'Membership', href: '/membership' },
+        {title: 'Accreditation', href: '/accreditation' },
         {title: 'Get involved', href: '/get-involved' },
         {title: 'IGPM Northern Ireland', href: '/more/igpm-northern-ireland' },
         {title: 'IGPM Wales', href: '/more/igpm-wales' },
@@ -73,7 +73,7 @@ export default function Footer(){
                                 <div className="flex gap-2 items-center text-[#484848] mt-3">
 
                                     <svg xmlns="http://www.w3.org/2000/svg" className="fill-[white]" height="1em" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" /></svg>
-                                    <div className="text-[#ADB6BB]">info@igpm.org</div>
+                                    <div className="text-[#ADB6BB]">info@igpm.org.uk</div>
 
                                 </div>
 
