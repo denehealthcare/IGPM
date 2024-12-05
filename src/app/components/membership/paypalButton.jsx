990c9f0b81380fee8109bf1fe9565abbcf70db49
TonyVipros <chris@gorillamail.co.uk>
2024-12-05 10:30:57 +0000

    Update paypalButton.jsx

--- src/app/components/membership/paypalButton.jsx
+++ src/app/components/membership/paypalButton.jsx
@@ -4,7 +4,7 @@ export default function PaypalButton(){
 
     return (
         <>
-            <Script  
+            {/* <Script  
                 src="https://www.paypal.com/sdk/js?client-id=BAAlGfbM8N1Lkb5nw26HO59BLpXaV53SgPVnV7yuhQIGjIcPTlhqW_h-A16bvk4OXhDKZec9adaXGGHbes&components=hosted-buttons&disable-funding=venmo&currency=GBP" id="show-paypal"
                 onReady={()=> {
                     paypal.HostedButtons({
@@ -12,7 +12,8 @@ export default function PaypalButton(){
                     }).render("#paypal-container-TFZMMVDAP37NL")
                 }}
                 />
-            <div id="paypal-container-TFZMMVDAP37NL"></div>
+            <div id="paypal-container-TFZMMVDAP37NL"></div> */}
+            <script src='https://app.glueup.com/compiled/js/eb-widgets.min.js' type='text/javascript'></script>
         </>
     )
 }
