f6a2734733c4b3c8fc583bc86efa0141c5e26bca
TonyVipros <chris@gorillamail.co.uk>
2024-12-05 11:20:57 +0000

    Update membership.jsx

--- src/app/components/membership/membership.jsx
+++ src/app/components/membership/membership.jsx
@@ -47,14 +47,7 @@ export default function Membership(){
 {/*                     <a target="_blank" className="w-[100%] sm:max-w-[500px] lg:w-[100%]" href="https://www.paypal.com/webapps/hermes?token=32R33450PE0069346&useraction=commit&wpsFlowRedirectToXorouterSkipHermesStartTime=1697029841117&mfid=1697029840826_f7309533ee478"><button className="bg-[#1B4E70] hover:bg-[#72C6ED] hover:text-[black] w-[100%] sm:max-w-[500px] lg:w-[100%] py-[8px] rounded-[8px] mt-3 text-white">Buy Now</button></a>
  */}
                     <div className="mt-5">
-                        {/*<PayPalButton />*/}
-                        
-<script src='https://app.glueup.com/compiled/js/eb-widgets.min.js' type='text/javascript'></script>
-
-<script>require(['https://app.glueup.com/compiled/js/eb-widgets.min']);</script>
-
-
-<iframe class="eb-widget" id="eb-widget-membership-type-list" name="eb-widget-membership-type-list" data-auto-adjust-height="true" data-auto-adjust-width="true" style="display:block; margin: 0; padding: 0; border: 0; outline: 0; font-size: 100%; vertical-align: baseline; background:transparent; " src="https://igpm.glueup.com/organization/6758/widget/membership-type-list/"></iframe>
+                        <PayPalButton />
                     </div>
 
                 </div>
