4460fa893cb73a5b392d1dae47c5bcabcb7ef871
TonyVipros <chris@gorillamail.co.uk>
2025-04-03 15:45:11 +0100

    Uploaded constitution

--- pages/about-igpm/igpm-constitution.js
+++ pages/about-igpm/igpm-constitution.js
@@ -12,7 +12,7 @@ export default function igpmConstitution() {
 
                         <h1 className="text-[#484848]">IGPM Constitution</h1>
                         <p className="mt-5 md:mt-6">
-                            <a href="#" target="_blank">IGPM Constitution Download</a>
+                            <a href="/downloads/NEW MEMORANDUM and ARTICLES OF ASSOCIATION for CIC limited by guarantee FI... 2025.docx" target="_blank" className="text-blue-700 hover:text-blue-950">IGPM Constitution Download</a>
                         </p>
                     </div>
                 </main>
