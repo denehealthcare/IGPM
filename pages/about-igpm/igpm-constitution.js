3307cb5919f22103e54c02699bd296ad05cb5868
TonyVipros <chris@gorillamail.co.uk>
2025-04-03 15:37:02 +0100

    Update igpm-constitution.js

--- pages/about-igpm/igpm-constitution.js
+++ pages/about-igpm/igpm-constitution.js
@@ -8,9 +8,13 @@ export default function igpmConstitution() {
         <Layout>
             <ResponsiveContainer>
                 <main className="mb-[70px] md:mb-[114px]">
-                    <p>
-                        <a href="#" target="_blank">IGPM Constitution Download</a>
-                    </p>
+                    <div className="mt-[140px] lg:mt-[200px]">
+
+                        <h1 className="text-[#484848]">IGPM Constitution</h1>
+                        <p className="mt-5 md:mt-6">
+                            <a href="#" target="_blank">IGPM Constitution Download</a>
+                        </p>
+                    </div>
                 </main>
             </ResponsiveContainer>
         </Layout>
